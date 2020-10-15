e83ff67a340e9d1863f59b6fa43c8e4a38efdb70
michael-methvin <mrmethvin12@gmail.com>
2020-10-15 15:33:33 -0500

    Adding the quiz grading to services

--- backend/src/main/java/me/quizzl/backend/services/QuizService.java
+++ backend/src/main/java/me/quizzl/backend/services/QuizService.java
@@ -1,13 +1,14 @@
 
 package me.quizzl.backend.services;
 
-import me.quizzl.backend.models.Quiz;
+import me.quizzl.backend.models.*;
 import me.quizzl.backend.repositories.QuizRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class QuizService {
@@ -19,7 +20,35 @@ public class QuizService {
         return quizRepository.save(quiz);
     }
 
+    public Quiz getQuizByID(UUID id) {
+        return quizRepository.getOne(id);
+    }
+
     public List<Quiz> getQuizzes() {
         return quizRepository.findAll();
     }
+    
+    public Grade gradeSubmission(Submission submission) {
+         
+         Grade grade = new Grade();
+         UUID quizId = submission.getQuizId();
+         Quiz quiz = getQuizByID(quizId);
+         List<Question> quizQuestions = quiz.getQuestions();
+         List<Answer> submissionAnswers = submission.getAnswers();
+         
+         for(int i = 0; i < quizQuestions.size(); i++) {
+             Boolean isCorrect = quizQuestions.get(i).evaluateAnswer(submissionAnswers.get(i));
+        
+            if(isCorrect) {
+                grade.incrementCorrect();
+            }
+            else {
+                 grade.incrementIncorrect();
+            }
+        }
+
+    return grade;
+    
+    }
 }
+
