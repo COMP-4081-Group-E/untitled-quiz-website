cc2b14a576ad77c20c76bd817673848fdb84bde1
CaptainKYem <45550848+CaptainKYem@users.noreply.github.com>
2020-10-16 17:59:20 -0500

    Update QuizService.java

--- backend/src/main/java/me/quizzl/backend/services/QuizService.java
+++ backend/src/main/java/me/quizzl/backend/services/QuizService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.UUID;
 import java.util.Optional;
+import java.lang.ResponseEntity;
 
 @Service
 public class QuizService {
