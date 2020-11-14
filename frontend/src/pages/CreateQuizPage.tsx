4c881b0438f8973afd716ac40b3de0662321060a
CaptainKYem <45550848+CaptainKYem@users.noreply.github.com>
2020-11-13 18:21:53 -0600

    redone

--- frontend/src/pages/CreateQuizPage.tsx
+++ frontend/src/pages/CreateQuizPage.tsx
@@ -3,13 +3,15 @@ import type { Question } from "../models/question";
 import Footer from "../Components/Footer";
 import QuestionForm from "../Components/QuestionForm";
 import MainLayout from "../Components/MainLayout";
-import {Button} from 'react-bootstrap';
+import { Alert, Button } from 'react-bootstrap';
+
 
 const { SNOWPACK_PUBLIC_API_URL } = import.meta.env;
 
 const CreateQuizPage = () => {
   const [title, setTitle] = useState<string>();
   const [questions, setQuestions] = useState<Question[]>([]);
+  const [link, setLink] = useState<string>();
 
   const updateTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
@@ -17,10 +19,6 @@ const CreateQuizPage = () => {
   const addQuestion = (question: Question) => {
     setQuestions([...questions, question]);
   };
-
-  const linkToQuiz = () => {
-    window.location.assign(`${window.location.hostname}/quiz/${id}`);
-  }
 
   const createQuiz = async () => {
     const res = await fetch(`${SNOWPACK_PUBLIC_API_URL}/api/quiz`, {
@@ -33,21 +31,26 @@ const CreateQuizPage = () => {
       return;
     }
 
+
     setQuestions([]);
-    linkToQuiz();
+    const id = await res.text;
+    setLink(`${window.location.protocol}//${window.location.hostname}/quiz/${id}`);
     // todo: probably don't use an alert for this in the future
     alert('Quiz created!');
   };
 
   return (
     <MainLayout>
+      {link && (
+        <Alert> Quiz at: {link}</Alert>
+      )}
       <h1>Create a Quiz</h1>
       <label>
         Quiz Title:
         <input type='text' onChange={updateTitle} />
       </label>
       <QuestionForm questionNumber={questions.length + 1} callback={addQuestion} />
-      <Button variant = "success" onClick={createQuiz}>Create Quiz</Button>
+      <Button variant="success" onClick={createQuiz}>Create Quiz</Button>
     </MainLayout>
   )
 }
